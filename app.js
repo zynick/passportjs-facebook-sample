2c97cf077506c3a63e565819a3534ca8c0b2d5ac
Chan Zi Yuan <ziyuan@zynick.com>
2016-12-15 20:58:25 +0800

    remove passport.js session stack

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
